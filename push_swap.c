9384cc3281510f35ce087343aa8170071c12432c
Peter Harbst <pharbst@student.42heilbronn.de>
2022-07-08 01:54:47 +0200

    init commit

---
+++ push_swap.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pharbst <pharbst@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/20 02:29:27 by pharbst           #+#    #+#             */
+/*   Updated: 2022/06/29 14:37:58 by pharbst          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include <stdarg.h>
+#include "./libft.library/libft.h"
+
+char ***ft_input_join(int argnum, char ***args)
+{
+	int		y;
+	char	*ret
+
+	y = 0;
+	while(--argnum > 0)
+		ret = ft_strjoinfree(ret, args[0][y++])
+}
+
+int main(int argc, char **args)
+{
+
+	if (argc == 1)
+		return (0);
+	else if (argc > 1)
+	{
+		if ()
+	}
+	// if (!ft_check_input)
+	// 	return (ft_printf("ERROR!"));
+	// put_input_together
+	// split input
+	// if (!ft_check_input)
+	// 	return (printf("ERROR!"));
+	// count elements
+	// idex elements
+	// init stacks
+	// while (!checksorted)
+	// 	midpoint algo
+	
+}
