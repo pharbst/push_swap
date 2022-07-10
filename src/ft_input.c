0249dfbe2445cdba8194e416f6714db2990a1f20
pharbst <pharbst@student.42heilbronn.de>
2022-07-10 12:18:04 +0200

    input handling started

--- src/ft_input.c
+++ src/ft_input.c
@@ -1,66 +1,49 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.c                                        :+:      :+:    :+:   */
+/*   ft_input.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pharbst <pharbst@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/20 02:29:27 by pharbst           #+#    #+#             */
-/*   Updated: 2022/07/10 12:08:10 by pharbst          ###   ########.fr       */
+/*   Created: 2022/07/09 14:16:22 by pharbst           #+#    #+#             */
+/*   Updated: 2022/07/10 12:16:28 by pharbst          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include <stdarg.h>
-#include "../libft/libft.h"
+#include "../includes/push_swap.h"
 
-char	**ft_inputsplit(char *src)
+t_stack	*ft_inputsplit(char *src)
 {
-	size_t	x;
-
-	x = 0;
-	while (src[x])
-	{
-
-	}
+	
 }
 
-char **ft_input(int argnum, char **args)
+int ft_inputcheck(char *format)
+{
+	if (!format)
+		return (1);
+	while (*format)
+	{
+		if (!ft_strchr("\t\n\r\f\v -+0123456789", *format))
+			return (1);
+		format++;
+	}
+	return (0);
+}
+
+t_stack	*ft_input(int argnum, char **args)
 {
 	int		y;
 	char	*ret;
 
 	ret = NULL;
-	y = 1;
+	y = 0;
 	while(--argnum > 0)
 	{
 		ret = ft_strjoinfree(ret, args[y++]);
 		ret = ft_strjoinfree(ret, " ");
 	}
 	if (ft_inputcheck(ret) == 1)
-		return (free(ret), NULL);
+		return (NULL);
 	else
 		return (ft_inputsplit(ret));
-}
-
-int main(int argc, char **args)
-{
-
-	if (argc == 1)
-		return (0);
-	else if (argc > 1)
-	{
-		if ()
-	}
-	// if (!ft_check_input)
-	// 	return (ft_printf("ERROR!"));
-	// put_input_together
-	// split input
-	// if (!ft_check_input)
-	// 	return (printf("ERROR!"));
-	// count elements
-	// idex elements
-	// init stacks
-	// while (!checksorted)
-	// 	midpoint algo
-	
 }
