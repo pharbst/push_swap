a07fa5e23de6ae007790b0737b546f0160d8ee1e
Peter Harbst <pharbst@student.42heilbronn.de>
2022-08-26 19:07:49 +0200

    the functions take the variables they need from the var struct instead of declaring theire own variable

--- src/tools/ft_chunk_midpoint.c
+++ src/tools/ft_chunk_midpoint.c
@@ -6,27 +6,26 @@
 /*   By: pharbst <pharbst@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/10 14:28:30 by pharbst           #+#    #+#             */
-/*   Updated: 2022/08/23 14:49:35 by pharbst          ###   ########.fr       */
+/*   Updated: 2022/08/26 17:33:40 by pharbst          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../push_swap.h"
 
-float	ft_chunk_midpoint(t_stack *stack)
+float	ft_chunk_midpoint(t_stack *stack, t_var *vars)
 {
-	int				chunkid;
 	t_stack			*node;
 	t_stack			*comparemin;
 	t_stack			*comparemax;
 
-	chunkid = stack->chunk;
+	vars->chunkid = stack->chunk;
 	node = stack;
 	comparemin = node;
 	comparemax = node;
 	while (node->next != stack)
 	{
 		node = node->next;
-		if (node->chunk == chunkid)
+		if (node->chunk == vars->chunkid)
 		{
 			if (node->index < comparemin->index)
 				comparemin = node;
--- src/tools/ft_chunkmembers.c
+++ src/tools/ft_chunkmembers.c
@@ -6,28 +6,26 @@
 /*   By: pharbst <pharbst@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/10 14:29:33 by pharbst           #+#    #+#             */
-/*   Updated: 2022/08/23 16:27:50 by pharbst          ###   ########.fr       */
+/*   Updated: 2022/08/26 18:00:43 by pharbst          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../push_swap.h"
 
-int	ft_chunkmembers(t_stack *stack)
+unsigned int	ft_chunkmembers(t_stack *stack, t_var *vars)
 {
-	int		chunkid;
-	int		chunkmembers;
-	t_stack	*node;
+	unsigned int	chunkmembers;
+	t_stack			*node;
 
 	node = stack;
 	chunkmembers = 0;
-	chunkid = stack->chunk;
 	while (stack->next != node)
 	{
-		if (stack->chunk == chunkid)
+		if (stack->chunk == vars->chunkid)
 			chunkmembers++;
 		stack = stack->next;
 	}
-	if (stack->chunk == chunkid)
+	if (stack->chunk == vars->chunkid)
 		chunkmembers++;
 	return (chunkmembers);
 }
