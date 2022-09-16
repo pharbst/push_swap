a8d07c8ca190dbbd7ff2e21d969e27132fd2f134
Peter Harbst <pharbst@student.42heilbronn.de>
2022-09-16 15:28:28 +0200

    header in ft_atoui

--- src/input/ft_atoui.c
+++ src/input/ft_atoui.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
+#include <push_swap.h>
 
 long long	ft_atoui(const char *src)
 {
